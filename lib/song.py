08ff5942226f1fc34db068767b9100b383b9521d
ckjolhede <ckjolhede@gmail.com>
2024-06-12 00:31:52 -0600

    done

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,35 @@
 class Song:
-    pass
+    count = 0
+    artists = []
+    genres = []
+    genre_count= {}
+    artist_count = {}
+
+        
+    @classmethod
+    def add_to_genres(cls, genre):
+        for genre in cls.genres:
+            cls.genre_count[genre] = cls.genres.count(genre)
+        return
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        for artist in cls.artists:
+            cls.artist_count[artist] = cls.artists.count(artist)
+        return
+            
+    @classmethod
+    def artist_count(cls):
+        return cls.artists_dict
+
+    
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count = Song.count + 1
+        Song.genres.append(genre)
+        Song.artists.append(artist)
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        
